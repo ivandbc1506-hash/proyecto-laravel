73dc59a6c22ecafb71f607860d27d2326e1603ca
ivandbc1506-hash <ivandbc1506@gmail.com>
2026-01-21 01:15:39 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/estudiantes', [EstudianteController::class, 'store'])->name('estudiantes.store');
     
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';

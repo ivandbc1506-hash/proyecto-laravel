2799a9035d866ce265bd4cd194c94a466c8b16e6
ivandbc1506-hash <ivandbc1506@gmail.com>
2026-01-21 01:36:36 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,8 +9,14 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
+    // Rutas de Estudiantes (Tu tabla)
     Route::get('/dashboard', [EstudianteController::class, 'index'])->name('dashboard');
     Route::post('/estudiantes', [EstudianteController::class, 'store'])->name('estudiantes.store');
+    
+    // RUTAS DE PERFIL: Son obligatorias para que no salga error
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';

37418d7d72110dbf604a5d72c9905e48d299616d
ivandbc1506-hash <ivandbc1506@gmail.com>
2026-01-21 01:55:19 -0500

    Update estudiantes.blade.php

--- resources/views/estudiantes.blade.php
+++ resources/views/estudiantes.blade.php
@@ -33,7 +33,7 @@
                 <h5 class="text-lg font-bold mb-4">Estudiantes Registrados</h5>
                 <table class="min-w-full table-auto border-collapse border border-green-500">
                     <thead>
-                        <tr class="bg-green-300 text-white">
+                        <tr class="bg-green-600 text-white">
                             <th class="border p-2">Nombre</th>
                             <th class="border p-2">Código</th>
                             <th class="border p-2">Carrera</th>
